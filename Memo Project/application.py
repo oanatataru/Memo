be917bae52cf2300847d31ec2db89ee6498ec31e
Tataru Oana-Vasilica <tataru.oana21@gmail.com>
2024-12-16 14:53:34 +0200

    Creare notita tip lista (grafica si implementare)

--- Memo Project/application.py
+++ Memo Project/application.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 
 import db
@@ -21,7 +22,10 @@ def load_notes():
         btn_frame.pack(side=TOP, pady=0, padx=0)
         id_label = Label(btn_frame, bg='#B93B3B', fg='white', text='#'+str(note[0]), font=('Verdana', 10, 'bold'))
         id_label.pack(side=LEFT, padx=(0, 75))
-        btn_delete = Button(btn_frame, text="X", command=lambda id_label=id_label: delete_note(id_label), padx=2, pady=0, bg="black", fg="white", font=('Verdana', 8, "bold"), relief="flat")
+        if note[-1] == 1:
+            btn_delete = Button(btn_frame, text="X", command=lambda id_label=id_label: delete_note(id_label), padx=2, pady=0, bg="black", fg="white", font=('Verdana', 8, "bold"), relief="flat")
+        else:
+            btn_delete = Button(btn_frame, text="X", padx=2, pady=0, bg="black", fg="white", font=('Verdana', 8, "bold"), relief="flat")
         btn_delete.pack(side=RIGHT, padx=(75, 0))
 
         title_label = Label(note_frame, bg='#B93B3B', fg='white', text=str(note[1]), anchor="w", font=('Verdana', 11, 'bold'), relief='flat')
@@ -30,11 +34,17 @@ def load_notes():
         content_text = Text(note_frame, wrap='word', width=40, height=6, bg='#B93B3B', fg='white', font=('Verdana', 9), relief='flat')
         if note[-1] == 1:
             content_text.insert('1.0', str(note[2]))
+        else:
+            tasks = json.loads(note[3])
+            for i, task in enumerate(tasks, start=1):
+                content_text.insert("end", f"{i}. {task}\n")
         content_text.configure(state='disabled')
         content_text.pack(side=TOP, padx=0, pady=10, anchor='w')
 
         if note[-1] == 1:
             btn_update = Button(note_frame, text='Modify', command=lambda id_label=id_label: note_popup(id_label), padx=2, pady=2, bg='black', fg='white', font=('Verdana', 10, 'bold'), relief='flat')
+        else:
+            btn_update = Button(note_frame, text='Modify', padx=2, pady=2, bg='black', fg='white', font=('Verdana', 10, 'bold'), relief='flat')
         btn_update.pack(side=BOTTOM, padx=0, pady=0)
 
 
@@ -92,6 +102,106 @@ def note_popup(param=None):
     btn_close.pack(side=LEFT, padx=68)
 
 
+def todo_list_popup():
+    def add_todo_list():
+        todo_title = field_title.get("1.0", "end-1c")
+        todo_tasks = [line.split(" ", 1)[1] for line in listbox_task.get(0, "end") if " " in line]
+        todo_tasks_json = json.dumps(todo_tasks)
+        db.add(todo_title, None, todo_tasks_json, 2)
+        popup.destroy()
+        load_notes()
+
+    def add_task():
+        task_string = field_task.get()
+        if len(task_string) == 0:
+            show_warning("Empty field!")
+        else:
+            tasks.append(task_string)
+            list_update()
+            field_task.delete(0, 'end')
+
+    def delete_task():
+        if len(tasks) == 0:
+            show_warning("No tasks to delete!")
+            return
+        try:
+            selected_task = " ".join(listbox_task.get(listbox_task.curselection()).split(" ")[1:])
+            if selected_task in tasks:
+                tasks.remove(selected_task)
+                list_update()
+        except:
+            show_warning("No task selected!")
+
+    def delete_all_tasks():
+        if len(tasks) == 0:
+            show_warning("No tasks to delete!")
+        else:
+            while len(tasks) != 0:
+                tasks.pop()
+            list_update()
+
+    def list_update():
+        clear_list()
+        for idx, task in enumerate(tasks, start=1):
+            listbox_task.insert('end', f"{idx}. {task}")
+
+    def clear_list():
+        listbox_task.delete(0, 'end')
+
+    def show_warning(warning):
+        field_task.configure(fg="red")
+        field_task.insert(0, warning)
+        popup.after(1000, clear_warning)
+
+    def clear_warning():
+        field_task.delete(0, 'end')
+        field_task.configure(fg="black")
+
+    tasks = []
+
+    popup = Toplevel(root)
+    popup.overrideredirect(True)
+    popup.geometry("580x550+500+182")
+    popup.config(bg='#B93B3B')
+
+    label_title = Label(popup, text="Tile", font=("Verdana", 12), bg='#B93B3B', fg='white', anchor="w")
+    label_title.pack(fill="x", pady=(60, 20), padx=60, anchor="w")
+    field_title = Text(popup, padx=10, pady=10, bg='#FFF8DC', fg='black', font=('Verdana', 12), height=1, )
+    field_title.pack(pady=0, fill=X, padx=60, anchor="w")
+
+    main_frame = Frame(popup, bg="#B93B3B")
+    main_frame.pack(expand=True, fill=BOTH)
+
+    functions_frame = Frame(main_frame, bg="#B93B3B",width=290, height=250)
+    listbox_frame = Frame(main_frame, bg="#B93B3B", width=290, height=250)
+    functions_frame.pack(side=LEFT, padx=0, pady=0)
+    listbox_frame.pack(side=RIGHT, padx=0, pady=0)
+
+    label_task = Label(functions_frame, text="Enter a task:", bg="#B93B3B", fg="white", font=("Verdana", 12))
+    label_task.place(x=60, y=0)
+    field_task = Entry(functions_frame, bd=5, relief="flat", bg="white", fg="black", font=("Verdana", 10), width=21)
+    field_task.place(x=60, y=40)
+
+    btn_add_task = Button(functions_frame, text="Add Task", command=add_task, bg="#A9A9A9", pady=3, font=("Verdana", 10), width=21, relief="flat")
+    btn_add_task.place(x=60, y=110)
+    btn_del_task = Button(functions_frame, text="Delete Task", command=delete_task, bg="#A9A9A9", pady=3, font=("Verdana", 10), width=21, relief="flat")
+    btn_del_task.place(x=60, y=160)
+    btn_del_all_tasks = Button(functions_frame, text="Delete All Tasks", command=delete_all_tasks, bg="#A9A9A9", pady=3, font=("Verdana", 10), width=21, relief="flat")
+    btn_del_all_tasks.place(x=60, y=210)
+
+    listbox_task = Listbox(listbox_frame, bd=10, width=26, height=12, selectmode='SINGLE', bg="white", fg="black", selectbackground="#FFF8DC", selectforeground="black", font=("Verdana", 10), relief="flat")
+    listbox_task.place(x=0, y=15)
+
+    btn_frame = Frame(popup, bg='#B93B3B')
+    btn_frame.pack(side=BOTTOM, pady=(20, 60), padx=60, anchor="w")
+
+    btn_save = Button(btn_frame, text="SAVE", command=add_todo_list, bd=1, bg="#2E7D32", fg="white", padx=20, pady=7, font=("Verdana", 10, "bold"), relief="solid")
+    btn_save.pack(side=LEFT, padx=68)
+
+    btn_close = Button(btn_frame, text="EXIT", command=popup.destroy, bd=1, bg="#B71C1C", fg="white", padx=15, pady=7, font=("Verdana", 10, "bold"), relief="solid")
+    btn_close.pack(side=LEFT, padx=68)
+
+
 
 db.db_setup()
 
@@ -106,7 +216,7 @@ app_btn_frame.pack(side=TOP, pady=60, padx=200, fill=X)
 btn_create_note = Button(app_btn_frame, text="Create new note...", command=note_popup, padx=70, pady=10, bg="#35373A", fg="white", font=('Verdana', 10), relief="flat")
 btn_create_note.pack(side=LEFT, padx=60)
 
-btn_create_todo_list = Button(app_btn_frame, text="Create new to-do list...", padx=50, pady=10, bg="#35373A", fg="white", font=('Verdana', 10), relief="flat")
+btn_create_todo_list = Button(app_btn_frame, text="Create new to-do list...", command=todo_list_popup, padx=50, pady=10, bg="#35373A", fg="white", font=('Verdana', 10), relief="flat")
 btn_create_todo_list.pack(side=LEFT)
 
 frame = Frame(root, width=800, height=350)
