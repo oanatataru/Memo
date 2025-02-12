0961204298feaccb4a2b76abbe160fe82faea9e0
Tataru Oana-Vasilica <126574637+oanatataru@users.noreply.github.com>
2025-02-12 18:07:35 +0200

    Delete Lab1 directory

--- Lab1/Exercise3.py
+++
@@ -1,9 +0,0 @@
-def count(main_string, substring):
-    return main_string.count(substring)
-
-
-first_string = input("Enter the first string: ")
-second_string = input("Enter the second string: ")
-
-occurrences = count(second_string, first_string)
-print(f"The number of occurrences of '{first_string}' in the second string is: {occurrences}")
--- Lab1/Exercise8.py
+++
@@ -1,5 +0,0 @@
-user_text = input("Enter a text in the console:")
-
-words_subtracted = user_text.split(sep=" ")
-
-print(len(words_subtracted))
